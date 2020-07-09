23eab2c27c7771b45ceecba6574e3bd6070155c6
Rawgher <rdpender3@gmail.com>
2020-07-09 14:02:21 -0700

    adding friend db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friends
+  has_many :friend, through: :friends
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
