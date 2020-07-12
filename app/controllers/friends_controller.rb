baf6475bd526ef62a763e10bc959d3a5ef8888ce
Rawgher <rdpender3@gmail.com>
2020-07-12 14:50:34 -0700

    set up friend follow

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,13 @@
 class FriendsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friends.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Friend added"
+    else
+      flash[:alert] = "Something went wrong"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
